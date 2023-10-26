4fb378282b6af9b518f0a6f3864cf7a52e18ad96
Meijirice <Mathieuphan@outlook.com>
2023-10-26 17:52:42 -0400

    Fixed up redirection

--- login.js
+++ login.js
@@ -1,27 +1,34 @@
-const valid_usernames = ["employee1", "employee2"];
-const valid_passwords = ["boo123", "foo123"];
+const valid_usernames_employees = ["employee1", "employee2"];
+const valid_passwords_employees = ["epass1", "epass2"];
+const valid_usernames_clients = ["client1", "client2"];
+const valid_passwords_clients = ["cpass1", "cpass2"];
 
 const employeeButton = document.getElementById("employee-login");
 const customerButton = document.getElementById("customer-login");
+const form = document.getElementById("login-form");
 
-
-function authenticate(){ 
+function authenticate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if(true){
-        window.location.assign("home.html");
-        alert("Logged in as " + username);
-    }
-    else{
-        alert("login unsuccessful");
+    let user_search = employeeButton.classList.contains('login-button-on') ? valid_usernames_employees : valid_usernames_clients;
+    let pass_search = employeeButton.classList.contains('login-button-on') ? valid_passwords_employees : valid_passwords_clients;
+    for (let i = 0; i < user_search.length; i++) {
+        if (user_search[i] == username && pass_search[i] == password) {
+                alert("Login successful");
+                window.location.assign("home.html");
+                return;
+        }
     }
 
+    alert("Wrong pass");
+    document.getElementById("login-form").reset();
 }
-employeeButton.addEventListener('click', function() {
+employeeButton.addEventListener('click', function () {
     employeeButton.classList.add('login-button-on');
     customerButton.classList.remove('login-button-on');
+
 });
-customerButton.addEventListener('click', function() {
+customerButton.addEventListener('click', function () {
     customerButton.classList.add('login-button-on');
     employeeButton.classList.remove('login-button-on');
 });

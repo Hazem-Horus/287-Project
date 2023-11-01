e93377374fb0a7a2fdfb72e48e29fd96a5759087
ItalyGuidi24 <122256507+ItalyGuidi24@users.noreply.github.com>
2023-11-01 12:59:10 -0400

    Update client dashboard(not finished).js

--- client dashboard(not finished).js
+++ client dashboard(not finished).js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const menuItems = document.querySelectorAll(".sidebar a");
     const contentSections = document.querySelectorAll(".content > div");
+    const editAccountButton = document.getElementById("edit-account");
 
     // Function to show content based on the selected menu item
     function showContent(event) {
@@ -9,12 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
         const content = getContent(target);
 
         // Hide all content sections
-        contentSections.forEach(section => {
-            section.style.display = 'none';
+        contentSections.forEach((section) => {
+            section.style.display = "none";
         });
 
         // Display selected content
-        content.style.display = 'block';
+        content.style.display = "block";
+    }
+
+    // Function to edit the account (simplified for demonstration)
+    function editAccount() {
+        const newAccountName = prompt("Enter the new account name:");
+        // Update the account name or perform necessary backend operations here
+        if (newAccountName) {
+            alert(`Account name updated to: ${newAccountName}`);
+        }
     }
 
     // Simulated content for menu items (replace with actual content)
@@ -30,23 +40,26 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Add click event listeners to menu items
-    menuItems.forEach(function(menuItem) {
+    menuItems.forEach(function (menuItem) {
         menuItem.addEventListener("click", showContent);
     });
 
     // Handle Edit Account, Delete Account, and Logout buttons
-    document.getElementById("edit-account").addEventListener("click", function() {
-        // Editing account functionality goes here
-        alert("Edit Account clicked. Functionality goes here");
+    editAccountButton.addEventListener("click", editAccount);
+
+    // Handle Delete Account button (simplified for demonstration)
+    document.getElementById("delete-account").addEventListener("click", function () {
+        if (confirm("Are you sure you want to delete your account?")) {
+            // Perform the account deletion process (backend logic) here
+            alert("Your account has been deleted.");
+        }
     });
 
-    document.getElementById("delete-account").addEventListener("click", function() {
-        // Delete account functionality goes here
-        alert("Delete Account clicked. Functionality goes here");
-    });
-
-    document.getElementById("logout").addEventListener("click", function() {
+    document.getElementById("logout").addEventListener("click", function () {
         // Logout account functionality goes here
         alert("Logout clicked. Functionality goes here");
     });
+
+    // Show dashboard content by default
+    document.getElementById("dashboard").click();
 });
